7d4c8ecc280529cfadb46952e079cf76261b047a
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-01-19 21:07:22 +0000

    feat: minimum tailwindcss project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecommerce
-Ecommerce clone in NextJS
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import reactLogo from './assets/react.svg'
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <h1>oi</h1>
+  )
+}
+
+export default App
