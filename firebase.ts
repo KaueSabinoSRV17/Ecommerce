73ca3e7d5e6d4fc6cf56f4465dabbb12f9236ff1
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-01-22 20:57:24 +0000

    refactor: removed unused import

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
